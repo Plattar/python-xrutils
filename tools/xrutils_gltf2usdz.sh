8bfd9fd9c2d1a6edbf4b56d46871b6852edf1cb9
David Arayan <david.arayan12@gmail.com>
2019-02-13 14:23:03 +1100

    - Added shell scripts to easily interface with the built/compiled conversion tools

---
+++ tools/xrutils_gltf2usdz.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Export our pathing information so the USDZ converter can function
+export PATH=$PATH:/usr/src/app/xrutils/USDPython/bin
+export PYTHONPATH=$PYTHONPATH:/usr/src/app/xrutils/USDPython/lib/python
+
+# This will enable a fix mode to ensure all special characters etc.. are
+# cleaned from the .gltf file itself
+python ./fixgltf.py $fromFile
+
+# Perform the conversion and save the output to the path that was
+# provided
+python ./../xrutils/gltf2usd/Source/gltf2usd.py -v -g $fromFile -o $toFile
---
+++ tools/xrutils_imgcheck.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Use the arcoreimg tool to perform a check against the provided image
+# file to see how suitable it is for marker-tracking purposes
+./../xrutils/arcoreimg/arcoreimg eval-img --input_image_path=$fromFile
