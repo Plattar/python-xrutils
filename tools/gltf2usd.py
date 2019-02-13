a2f33230a2c50427acdb274f6afffc4e3a276254
David Arayan <david.arayan12@gmail.com>
2019-02-13 16:41:34 +1100

    - Renamed the gltf2usdz and shell equivalent to gltf2usd - gltf2usd.py will now also accept the other usd file formats instead of forcing only .usdz, this makes the tool a bit more versatile

---
+++ tools/gltf2usd.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import subprocess
+import shlex
+import sys
+import os
+
+# Ensure we only accept 3 command line arguments for [script, source, destination]
+if len(sys.argv) != 3:
+	print "gltf2usd.py -> script requires a source and destination files"
+	print "gltf2usd.py -> cannot proceed, exit"
+	sys.exit()
+
+src_file = sys.argv[1]
+des_file = sys.argv[2]
+
+# We perform some prelimenary quick checks to ensure all our attributes
+# are satisfied before we execute expensive programs
+src_file_name, src_file_extension = os.path.splitext(src_file)
+dst_file_name, dst_file_extension = os.path.splitext(des_file)
+
+# The source file must always be .gltf
+if src_file_extension.lower() != '.gltf':
+	print "gltf2usd.py -> source file must have an extension of .gltf but was -> " + src_file_extension
+	print "gltf2usd.py -> cannot proceed, exit"
+	sys.exit()
+
+# The destination file must always be .usdz
+if (dst_file_extension.lower() in ['.usdz','.usdc','.usda','usd']) == False:
+	print "gltf2usd.py -> destination file must have an extension of .usdz, .usdc, .usda or .usd but was -> " + dst_file_extension
+	print "gltf2usd.py -> cannot proceed, exit"
+	sys.exit()
+
+# Check if the source file exists at path
+if os.path.isfile(src_file) != True:
+	print "gltf2usd.py -> script requires the source file to exist -> " + src_file
+	print "gltf2usd.py -> cannot proceed, exit"
+	sys.exit()
+
+print "gltf2usd.py -> setting source file path -> " + src_file
+print "gltf2usd.py -> setting destination file path -> " + des_file
+
+xrutils_command = "sh xrutils_gltf2usd.sh " + src_file + " " + des_file
+
+print "gltf2usd.py -> executing command -> " + xrutils_command
+
+# call our shell script, which will in turn execute the appropriate application
+subprocess.call(shlex.split(xrutils_command))
